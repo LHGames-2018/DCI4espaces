edb1000e2943b950ecf08e5be54cd8975f46e2b6
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-10-06 12:41:42 -0400

    Move towards an point. If something is encounted, do another action instead.

--- bot/pathingActions.py
+++ bot/pathingActions.py
@@ -5,10 +5,13 @@ from helper.aiHelper import *
 class PathingActions:
 
     @staticmethod
-    def destroyInPath(gameMap, playerInfo, move):
+    def doActionInPath(gameMap, playerInfo, move, action):
         nextPosition = Point(playerInfo.x + move.x, playerInfo.y + move.y)
         # If there is a wall at the given position, destroy it.
         if gameMap.getTileAt(nextPosition) == TileContent.Wall:
-            return create_attack_action(move)
+            return action(move)
         else:
             return create_move_action(move)
+
+
+    
