d346df8cb7d68d08b8e0dac756a93149e6c50b50
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-10-06 12:26:57 -0400

    MapHelper.MoveTowards allows to get the next point to get to something. It can be used to determine in which direction to do an action.

---
+++ helper/mapHelper.py
@@ -0,0 +1,23 @@
+from structs import Point
+
+class MapHelper:
+
+    # Helper method to determine what is the next move in order to move
+    # towards something. It uses manhattan movement in order to retrieve a point.
+    # Ex: Point(-1, 0) to move left.
+    @staticmethod
+    def getMoveTowards(fromPoint, toPoint):
+        distX = toPoint.x - fromPoint.x
+        distY = toPoint.y - fromPoint.y
+
+        absoluteDistX = abs(distX)
+        absoluteDistY = abs(distY)
+        
+        # If we are closer in X position, move in the y axis
+        if absoluteDistX <= absoluteDistY and distY != 0:
+            return Point(0, distY/absoluteDistY) # abs is used to keep the sign on the move.
+        elif absoluteDistX > absoluteDistY and distX != 0:
+            return Point(distX/absoluteDistX, 0)
+        else:
+            print("[MapHelper.getMoveTowards] You are already at the given position.")
+            return Point(0, 0)
