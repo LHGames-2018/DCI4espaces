9b871b3da95adb73cf70f5fb0ada6e8c2e210dca
Mathieu Dumoulin <mathieu.dumoulin@outlook.com>
2018-10-06 12:49:01 -0400

    Action si possibl, sinon move.

--- bot/pathingActions.py
+++ bot/pathingActions.py
@@ -4,14 +4,18 @@ from helper.aiHelper import *
 
 class PathingActions:
 
+    # Move the player towards a point. If a "triggeringTileContent" is encountered,
+    # change the action of the player from moving to "action".
+    # "point" must be a unit vector, ex: Point(-1, 0) for left.
     @staticmethod
-    def doActionInPath(gameMap, playerInfo, move, action):
-        nextPosition = Point(playerInfo.x + move.x, playerInfo.y + move.y)
+    def doActionInPath(gameMap, playerInfo, point, triggeringTileContent, action):
+        nextPosition = Point(playerInfo.x + point.x, playerInfo.y + point.y)
         # If there is a wall at the given position, destroy it.
-        if gameMap.getTileAt(nextPosition) == TileContent.Wall:
-            return action(move)
+        if gameMap.getTileAt(nextPosition) == triggeringTileContent:
+            return action(point)
         else:
-            return create_move_action(move)
+            return create_move_action(point)
+
 
 
     
